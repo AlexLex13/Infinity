fc02a0127ae4879881e455e8acddd475655724dc
Alexey <suxovarov.alexey@gmail.com>
2023-12-03 16:07:35 +0300

    Configure http.Server

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"context"
 	"github.com/AlexLex13/Infinity/internal/config"
+	"github.com/AlexLex13/Infinity/internal/http-server/handlers/save"
 	"github.com/AlexLex13/Infinity/internal/http-server/middleware/logger"
 	"github.com/AlexLex13/Infinity/internal/lib/logger/sl"
 	"github.com/AlexLex13/Infinity/internal/storage/sqlite"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"log/slog"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 const (
@@ -21,9 +27,8 @@ func main() {
 	cfg := config.MustLoad()
 
 	log := setupLogger(cfg.Env)
-	log = log.With(slog.String("env", cfg.Env)) // к каждому сообщению будет добавляться поле с информацией о текущем окружении
 
-	log.Info("initializing server", slog.String("address", cfg.Address)) // Помимо сообщения выведем параметр с адресом
+	log.Info("initializing server", slog.String("env", cfg.Env))
 	log.Debug("logger debug mode enabled")
 
 	storage, err := sqlite.New(cfg.StoragePath)
@@ -31,14 +36,50 @@ func main() {
 		log.Error("failed to initialize storage", sl.Err(err))
 	}
 
-	log.Info("Storage", storage)
-
 	router := chi.NewRouter()
 
 	router.Use(middleware.RequestID) // Добавляет request_id в каждый запрос, для трейсинга
 	router.Use(middleware.Recoverer) // Если где-то внутри сервера (обработчика запроса) произойдет паника, приложение не должно упасть
 	router.Use(middleware.URLFormat) // Парсер URLов поступающих запросов
+	router.Use(middleware.Logger)
 	router.Use(logger.New(log))
+
+	router.Post("/", save.New(log, storage))
+
+	log.Info("Server starting", slog.String("address", cfg.Address))
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	srv := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Error("failed to start server")
+		}
+	}()
+
+	log.Info("server started")
+
+	<-done
+	log.Info("stopping server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Error("failed to stop server", sl.Err(err))
+
+		return
+	}
+
+	log.Info("server stopped")
 }
 
 func setupLogger(env string) *slog.Logger {
